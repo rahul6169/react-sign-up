a7c19fcba94d55820419c1c6a8f481cf1ef50d9d
RAHUL R B <rahulbalakrishnan6169@gmail.com>
2022-12-27 15:39:11 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import './App.css';
-import Signup from './message.js';
-
+import "./App.css";
+import Signup from "./message.js";
+import "./message.css";
 function App() {
   return (
-<div className='rahul'>
-
-  <Signup/>
-  
-</div>
-    
-  
+    <div className="rahul">
+      <Signup />
+    </div>
   );
-  
 }
 
 export default App;
