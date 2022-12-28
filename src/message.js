7e32d2c277c8d3a108d24892e44d941085325f6e
RAHUL R B <rahulbalakrishnan6169@gmail.com>
2022-12-28 12:27:52 +0530

    javascript

--- src/message.js
+++ src/message.js
@@ -1,193 +1,295 @@
+import React from "react";
+import { useState } from "react";
+
 const Signup = () => {
-  const onChange = (e) => {
-    console.log("working");
-    console.log(e.target.value);
-  };
+  const [form, setForm] = useState({
+    userName: "",
+    email: "",
+    number: "",
+    contactName: "",
+    contactEmail: "",
+    contactPhoneNumber: "",
+    type: "",
+    percent: "",
+    dateInput: "",
+  });
+  function handleClick() {
+    // console.log(form);
+    // setForm(form.value);
+  }
+
+  console.log(form);
+
   return (
-    <body>
-      <form id="newBusiness">
-        <h2>New Business</h2>
+    <form onSubmit={handleClick} id="newBusiness">
+      <h2>New Business</h2>
 
-        <div>
-          <label>User Name </label>
+      <div>
+        <label>User Name </label>
+        <input
+          type="text"
+          value={form.userName}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              userName: e.target.value,
+            });
+          }}
+          id="userName"
+          name="userName"
+          placeholder="Enter Your Name"
+        ></input>
+      </div>
+
+      <div>
+        <label>Email </label>
+        <input
+          type="email"
+          value={form.email}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              email: e.target.value,
+            });
+          }}
+          id="email"
+          placeholder="Enter Your Email"
+        ></input>
+      </div>
+
+      <div>
+        <label>Phone Number</label>
+        <input
+          type="tel"
+          value={form.number}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              number: e.target.value,
+            });
+          }}
+          id="phoneNumber"
+          minLength={5}
+          maxLength={10}
+          placeholder="Enter Your Number"
+        ></input>
+      </div>
+
+      <div>
+        <label>Contact Name </label>
+        <input
+          type="text"
+          value={form.contactName}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              contactName: e.target.value,
+            });
+          }}
+          id="contactName"
+          placeholder="Enter Your Name"
+        ></input>
+      </div>
+
+      <div>
+        <label>Email </label>
+        <input
+          type="email"
+          value={form.contactEmail}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              contactEmail: e.target.value,
+            });
+          }}
+          id="contactEmail"
+          placeholder="Enter Contact Email"
+        ></input>
+      </div>
+
+      <div>
+        <label>Phone Number</label>
+        <input
+          type="tel"
+          value={form.contactPhoneNumber}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              contactPhoneNumber: e.target.value,
+            });
+          }}
+          id="contactPhoneNumber"
+          minLength={5}
+          maxLength={10}
+          placeholder="Enter Contact Number"
+        ></input>
+      </div>
+
+      <div>
+        <label>
+          Type <br></br>
           <input
-            type="text"
-            id="userName"
-            // onChange={onChange}
-            placeholder="Enter Your Name"
-          ></input>
-        </div>
-
-        <div>
-          <label>Email </label>
-          <input type="email" id="email" placeholder="Enter Your Email"></input>
-        </div>
-
-        <div>
-          <label>Phone Number</label>
+            type="radio"
+            value={form.type}
+            onChange={(e) => {
+              setForm({
+                ...form,
+                type: e.target.value,
+              });
+            }}
+            id="smallBusiness"
+            name="Business"
+            value="smallBusiness"
+          />
+          <label for="smallBusiness">Small Business </label>
+          <br></br>
           <input
-            type="tel"
-            id="phoneNumber"
-            minLength={5}
-            maxLength={10}
-            placeholder="Enter Your Number"
-          ></input>
-        </div>
-
-        <div>
-          <label>Contact Name </label>
+            type="radio"
+            type="radio"
+            value={form.type}
+            onChange={(e) => {
+              setForm({
+                ...form,
+                type: e.target.value,
+              });
+            }}
+            id="enterprise"
+            name="Business"
+            value="Enterprise"
+          />
+          <label for="enterprise">Enterprise</label>
+          <br></br>
           <input
-            type="text"
-            id="contactName"
-            // onChange={onChange}
-            placeholder="Enter Your Name"
-          ></input>
-        </div>
+            type="radio"
+            type="radio"
+            value={form.type}
+            onChange={(e) => {
+              setForm({
+                ...form,
+                type: e.target.value,
+              });
+            }}
+            id="entrepreneur"
+            name="Business"
+            value="Entrepreneur"
+          />
+          <label for="entrepreneur">Entrepreneur</label>
+        </label>
+      </div>
 
-        <div>
-          <label>Email </label>
+      <div>
+        <label for="catogory" class="catogoryy">
+          Catogory<br></br>
+          <select id="catogory" multiple size="5">
+            <option name="catogory" value="toys">
+              Toys
+            </option>
+            <option name="catogory" value="clothes">
+              Clothes
+            </option>
+            <option name="catogory" value="groceries">
+              groceries
+            </option>
+            <option name="catogory" value="Electronics">
+              Electronics
+            </option>
+            <option name="catogory" value="Digital">
+              Digital
+            </option>
+          </select>
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Percent
           <input
-            type="email"
-            id="contactEmail"
-            placeholder="Enter Contact Email"
+            type="radio"
+            value={form.percent}
+            onChange={(e) => {
+              setForm({
+                ...form,
+                percent: e.target.value,
+              });
+            }}
+            type="number"
+            id="percent"
+            max="100"
+            size="3"
           ></input>
-        </div>
+        </label>
+      </div>
 
-        <div>
-          <label>Phone Number</label>
+      <div>
+        <label>
+          Active from
           <input
-            type="tel"
-            id="contactPhoneNumber"
-            minLength={5}
-            maxLength={10}
-            placeholder="Enter Contact Number"
+            type="radio"
+            value={form.dateInput}
+            onChange={(e) => {
+              setForm({
+                ...form,
+                dateInput: e.target.value,
+              });
+            }}
+            type="date"
+            id="activefrom"
           ></input>
-        </div>
+        </label>
+      </div>
 
-        <div>
-          <label>
-            Type <br></br>
-            <input
-              type="radio"
-              id="smallBusiness"
-              name="Business"
-              value="smallBusiness"
-            />
-            <label for="smallBusiness">Small Business </label>
-            <br></br>
-            <input
-              type="radio"
-              id="enterprise"
-              name="Business"
-              value="Enterprise"
-            />
-            <label for="enterprise">Enterprise</label>
-            <br></br>
-            <input
-              type="radio"
-              id="entrepreneur"
-              name="Business"
-              value="Entrepreneur"
-            />
-            <label for="entrepreneur">Entrepreneur</label>
-          </label>
-        </div>
+      <div>
+        <label class="payments">
+          Payment Option<br></br>
+          <input
+            type="checkbox"
+            name="payment"
+            value="Cash on delivery"
+            id="cashOnDelivery"
+            placeholder="cash on delivery"
+          />
+          <label for="cashOnDelivery">Cash on delivery</label>
+          <br></br>
+          <input
+            type="checkbox"
+            name="payment"
+            value="UPI"
+            id="UPI"
+            placeholder="payment"
+          />
+          <label for="UPI">UPI</label>
+          <br></br>
+          <input
+            type="checkbox"
+            name="payment"
+            value="card payment"
+            id="cardPayment"
+            placeholder="cardPayment"
+          />
+          <label for="cardPayment">Card Payment</label>
+        </label>
+      </div>
 
-        <div>
-          <label for="catogory" class="catogoryy">
-            Catogory<br></br>
-            <select id="catogory" multiple size="5">
-              <option name="catogory" value="toys">
-                Toys
-              </option>
-              <option name="catogory" value="clothes">
-                Clothes
-              </option>
-              <option name="catogory" value="groceries">
-                groceries
-              </option>
-              <option name="catogory" value="Electronics">
-                Electronics
-              </option>
-              <option name="catogory" value="Digital">
-                Digital
-              </option>
-            </select>
-          </label>
-        </div>
+      <div>
+        <label>
+          Notes<br></br>
+          <textarea id="textArea" cols={30} rows="5"></textarea>
+        </label>
+      </div>
 
-        <div>
-          <label>
-            Percent
-            <input type="number" id="percent" max="100" size="3"></input>
-          </label>
-        </div>
+      <div>
+        <label>
+          Critical Account<br></br>
+          <input type="checkbox" id="criticalCheckBox"></input>
+        </label>
+      </div>
 
-        <div>
-          <label>
-            Active from
-            <input type="date" id="activefrom"></input>
-          </label>
-        </div>
-
-        <div>
-          <label class="payments">
-            Payment Option<br></br>
-            <input
-              type="checkbox"
-              name="payment"
-              value="Cash on delivery"
-              id="cashOnDelivery"
-              placeholder="cash on delivery"
-            />
-            <label for="cashOnDelivery">Cash on delivery</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="payment"
-              value="UPI"
-              id="UPI"
-              placeholder="payment"
-            />
-            <label for="UPI">UPI</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="payment"
-              value="card payment"
-              id="cardPayment"
-              placeholder="cardPayment"
-            />
-            <label for="cardPayment">Card Payment</label>
-          </label>
-        </div>
-
-        <div>
-          <label>
-            Notes<br></br>
-            <textarea id="textArea" cols={30} rows="5"></textarea>
-          </label>
-        </div>
-
-        <div>
-          <label>
-            Critical Account<br></br>
-            <input type="checkbox" id="criticalCheckBox"></input>
-          </label>
-        </div>
-
-        <div>
-          <button
-            type="submit"
-            onClick={onChange}
-            id="submitButton"
-            value="Submit"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    </body>
+      <div>
+        <button type="submit" id="submitButton" value="Submit">
+          Submit
+        </button>
+      </div>
+    </form>
   );
 };
 export default Signup;
